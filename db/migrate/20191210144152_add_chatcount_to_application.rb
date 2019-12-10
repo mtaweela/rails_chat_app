44e5664df0066464eb664c81ede17dc0e0373e30
mtaweela <mohamed.taweela56@gmail.com>
2019-12-10 16:51:15 +0200

    -- remove wrong column from application tanle -- edit to docker-compose

--- db/migrate/20191210144152_add_chatcount_to_application.rb
+++
@@ -1,5 +0,0 @@
-class AddChatcountToApplication < ActiveRecord::Migration[5.2]
-  def change
-    add_column :applications, :chat_token, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_144152) do
+ActiveRecord::Schema.define(version: 2019_12_10_143326) do
 
   create_table "applications", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_12_10_144152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
-    t.integer "chat_token"
     t.index ["token"], name: "index_applications_on_token", unique: true
   end
 
